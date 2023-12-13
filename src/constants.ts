42818b0d9a416e8d1e8e37db4dd8912827e36701
Nico Brinkkemper <nico@brinkkemper.nl>
2023-12-13 03:54:04 +0100

    add public url

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
 export const SNAP = navigator.userAgent !== "ReactSnap"
-export const BASE_URL =
-  navigator.userAgent !== "ReactSnap"
-    ? "https://localhost:3000"
-    : 'https://mmcelebration.com';
+export const BASE_URL = 'https://mmcelebration.com';
 export const DEFAULT_DESCRIPTION = 'Yearly Mario Maker Celebrations\' Official Site'
