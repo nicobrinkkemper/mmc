2b9dd71a020ee2cd3197da43b8d61b361fa192f5
Nico Brinkkemper <nico@brinkkemper.nl>
2023-12-27 20:04:26 +0100

    clean up public url stuff

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ export const SNAP = navigator?.userAgent !== "ReactSnap";
 export const DEFAULT_DESCRIPTION =
   "Yearly Mario Maker Celebrations' Official Site";
 export const PUBLIC_URL = process.env.PUBLIC_URL ?? "";
-export const BASE_URL = process.env.REACT_APP_BASE_URL ?? "";
+export const BASE_URL =
+  process.env.REACT_APP_BASE_URL ?? "https://mmcelebration.com";
