b8caa5165fbf3c38cb0fb16027c4e76c8c790184
Nico Brinkkemper <nico@brinkkemper.nl>
2023-12-27 19:59:58 +0100

    base url

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const SNAP = navigator?.userAgent !== "ReactSnap";
 export const DEFAULT_DESCRIPTION =
   "Yearly Mario Maker Celebrations' Official Site";
 export const PUBLIC_URL = process.env.PUBLIC_URL ?? "";
-export const BASE_URL = (process.env.REACT_APP_BASE_URL ?? "") + PUBLIC_URL;
+export const BASE_URL = process.env.REACT_APP_BASE_URL ?? "";
