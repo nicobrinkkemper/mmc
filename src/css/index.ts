997313c60e88cd7c4a6f0364e3689e12a0ee6604
Nico Brinkkemper <nico@brinkkemper.nl>
2025-12-10 16:04:09 +0100

    more 10mmc folders

--- src/css/index.ts
+++ src/css/index.ts
@@ -3,4 +3,6 @@ export { default as _5ymm, default as _6ymm } from "./5-6ymm.module.css";
 export { default as _7mmc } from "./7mmc.module.css";
 export { default as _8mmc } from "./8mmc.module.css";
 export { default as _9mmc } from "./9mmc.module.css";
+//
+export { default as _10mmc } from "./10mmc.module.css";
 
